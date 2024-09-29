5d7eebf8a98b9370da5d4b0aff1db5630fa7deed
zhengcr <zhengcr@eastcom-sw.com>
2024-09-29 17:59:17 +0800

    feat：打包

--- deploy.sh
+++ deploy.sh
@@ -20,6 +20,8 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
 
 # 如果发布到 https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:S-viking/ch-ui.git master:gh-pages
+# git push -f git@github.com:S-viking/ch-ui.git master:gh-pages
+
+git push -f https://github.com/S-viking/ch-ui.git master:gh-pages
 
 cd -
